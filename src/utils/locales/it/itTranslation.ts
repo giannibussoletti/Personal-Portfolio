b770f872bd824266cd822a4a52d5418c37fd5758
giannibussoletti <giannibussoletti@gmail.com>
2026-09-24 22:26:58 +0200

    updates 🦝 :)

--- src/utils/locales/it/itTranslation.ts
+++ src/utils/locales/it/itTranslation.ts
@@ -2,7 +2,7 @@ export default {
   it: {
     translation: {
       profile: {
-        salute: "Ciao! 🦝",
+        salute: "Ciao🦝!",
         description:
           "Sono Gianni, sviluppatore full-stack junior di Roma. Vengo dal mondo della grafica e del video e dal 2018 sviluppo anche siti WordPress. Oggi creo applicazioni web con React, TypeScript, Java e Spring Boot. L'occhio per il design e la cura del dettaglio li porto in ogni progetto che sviluppo, dal front-end al back-end.",
         quote: "Amo le storie ben raccontate, sullo schermo come nel codice",
