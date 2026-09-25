16512a0d0f598b9ba4d07b893bbe443e40cdffd2
giannibussoletti <giannibussoletti@gmail.com>
2026-09-25 13:39:47 +0200

    optimized fontawesome library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-// FONTAWESOME
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { fas } from "@fortawesome/free-solid-svg-icons"
-import { far } from "@fortawesome/free-regular-svg-icons"
-import { fab } from "@fortawesome/free-brands-svg-icons"
-library.add(fas, far, fab)
-
 import { Container, Row, Col } from "react-bootstrap"
 
 import { useTranslation } from "react-i18next"
